0760261387338e4f883b21376e4ff122f8863906
rmgal <renov.gorbachev@gmail.com>
2025-05-24 08:33:11 -0700

    reverted addToCart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,34 +5,20 @@ const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  // const addToCart = (product, quantity = 1) => {
-  //   const existing = cart.find((item) => item._id === product._id);
-
-  //   if (existing) {
-  //     setCart(
-  //       cart.map((item) =>
-  //         item._id === product._id
-  //           ? { ...item, quantity: item.quantity + quantity }
-  //           : item
-  //       )
-  //     );
-  //   } else {
-  //     setCart([...cart, { ...product, quantity }]);
-  //   }
-  // };
   const addToCart = (product, quantity = 1) => {
-    setCart((prevCart) => {
-      const existing = prevCart.find((item) => item._id === product._id);
-      if (existing) {
-        return prevCart.map((item) =>
+    const existing = cart.find((item) => item._id === product._id);
+
+    if (existing) {
+      setCart(
+        cart.map((item) =>
           item._id === product._id
-            ? { ...item, quantity: item.quantity + product.quantity }
+            ? { ...item, quantity: item.quantity + quantity }
             : item
-        );
-      } else {
-        return [...prevCart, { ...product, quantity }];
-      }
-    });
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, quantity }]);
+    }
   };
 
 
