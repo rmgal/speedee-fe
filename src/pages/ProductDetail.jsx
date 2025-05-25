3dbeda37e68bc185d2f8146047d5e5c301cec7f3
rmgal <renov.gorbachev@gmail.com>
2025-05-25 07:40:13 -0700

    fix add to cart ties with qty

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -63,14 +63,10 @@ const ProductDetail = () => {
           className="w-20 border rounded px-3 py-1 mr-3"
         />
         <button
-          onClick={handleAddToCart}
-          className="bg-blue-600 text-white px-6 py-3 rounded"
-        >
-        {/* <button
             //   onClick={() => addToCart(product, quantities[product._id])}
             onClick={() => addToCart({ ...product, quantity: quantities[product._id] || 1 })}
             className="bg-blue-600 text-white px-6 py-3 rounded"
-        > */}
+        >
           Add to Cart
         </button>
       </div>
